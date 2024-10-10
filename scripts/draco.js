dfe53badef86d241e570963711001a8a77cb5ae2
Charlesss-0 <aragonaugusto588@gmail.com>
2024-10-10 17:39:45 -0600

    fix(draco): fix linter issues
    - fix dependencies issue

--- scripts/draco.js
+++ scripts/draco.js
@@ -1,13 +1,13 @@
-const fs = require('fs-extra')
+import fs from 'fs-extra'
 
 const src = 'node_modules/three/examples/jsm/libs/draco/gltf'
 const output = 'public/draco'
 
 // Copy draco decoder from three.js into the public directory
 fs.copy(`${src}/draco_decoder.wasm`, `${output}/draco_decoder.wasm`, err => {
-	if (err) return console.error(err)
+	if (err) throw new Error(err)
 })
 
 fs.copy(`${src}/draco_wasm_wrapper.js`, `${output}/draco_wasm_wrapper.js`, err => {
-	if (err) return console.error(err)
+	if (err) throw new Error(err)
 })
