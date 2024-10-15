df354607fde807757bc1d14abda0010d5be9daeb
Charlesss-0 <aragonaugusto588@gmail.com>
2024-10-15 10:06:05 -0600

    refactor(mouseAnimation): use gsap for wheel animation

--- src/components/animations/mouse-animation.tsx
+++ src/components/animations/mouse-animation.tsx
@@ -1,38 +1,33 @@
-import styled, { keyframes } from 'styled-components'
-
-const moveDown = keyframes`
-    25% {
-        transform: translateY(0) translateX(-1px);
-        opacity: 1;
-    }
-
-    75% {
-        transform: translateY(5px) translateX(-1px);
-        opacity: 0;
-    }
-`
-
-const Mouse = styled.div`
-	border: 3px solid #efefef;
-	border-radius: 20px;
-	width: 26px;
-	height: 38px;
-
-	&::before {
-		content: '';
-		height: 7px;
-		width: 2px;
-		background: #efefef;
-		border-radius: 4px;
-		position: absolute;
-		top: 6px;
-		left: 50%;
-		transform: translateX(-50%);
-
-		animation: ${moveDown} 2s infinite;
-	}
-`
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { useRef } from 'react'
 
 export default function MouseAnimation(): React.ReactNode {
-	return <Mouse />
+	const wheel = useRef<HTMLDivElement>(null)
+
+	useGSAP(() => {
+		gsap.fromTo(
+			wheel.current,
+			{
+				duration: 2,
+				y: 0,
+				opacity: 1,
+			},
+			{
+				duration: 2,
+				y: 5,
+				opacity: 0,
+				repeat: -1,
+			}
+		)
+	})
+
+	return (
+		<div className="border-[3px] border-neutral-200 rounded-[20px] w-[26px] h-[38px] relative">
+			<div
+				ref={wheel}
+				className="absolute h-[7px] w-[2px] bg-light-blue rounded-[4px] top-[6px] left-[50%] translate-x-[-50%]"
+			/>
+		</div>
+	)
 }
