b68a254a5cec753edd044ccaeb4827bcb0cea5f4
Charlesss-0 <aragonaugusto588@gmail.com>
2024-10-30 09:49:15 -0600

    remove security headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
@@ -8,31 +10,6 @@ const nextConfig = {
 		removeConsole: process.env.NODE_ENV !== 'development',
 	},
 	transpilePackages: ['three'],
-	async headers() {
-		return [
-			{
-				key: 'Content-Security-Policy',
-				value:
-					"default-src 'self'; img-src *; media-src media1.com media2.com; script-src userscripts.example.com",
-			},
-			{
-				key: 'X-Frame-Options',
-				value: 'SAMEORIGIN',
-			},
-			{
-				key: 'X-Content-Type-Options',
-				value: 'nosniff',
-			},
-			{
-				key: 'Referrer-Policy',
-				value: 'strict-origin-when-cross-origin',
-			},
-			{
-				key: 'Permissions-Policy',
-				value: 'geolocation=(self), microphone=()',
-			},
-		]
-	},
 }
 
 export default nextConfig
