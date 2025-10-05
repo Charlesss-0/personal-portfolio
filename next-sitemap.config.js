10703dca627dfb173cd1fffbeff53352b2facbb2
Charlesss-0 <aragonaugusto588@gmail.com>
2025-10-05 17:44:37 -0600

    chore: remove next-sitemap and add standard sitemap generator from Next.js

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-	siteUrl: 'https://carlosaragondev.vercel.app',
-	generateRobotsTxt: true,
-	robotsTxtOptions: {
-		policies: [{ userAgent: '*', allow: '/' }],
-	},
-}
-
-module.exports = config
